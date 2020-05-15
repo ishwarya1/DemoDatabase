4476a9bbfbf80d4b5caacb901453fabbb2b0f0f2
18016638 <18016638@myrp.edu.sg>
2020-05-15 14:51:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/soi/demodatabase/MainActivity.java
@@ -0,0 +1,86 @@
+package sg.edu.rp.soi.demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnInsert,btnGetTasks;
+    TextView tvResults;
+ListView lv;
+ArrayAdapter aa;
+ArrayList<Task> task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnGetTasks = findViewById(R.id.btngettasks);
+        lv = findViewById(R.id.lv);
+        tvResults = findViewById(R.id.tvresults);
+        btnInsert = findViewById(R.id.btninsert);
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                task = db.getTasks();
+
+
+
+                aa = new TaskAdapter(MainActivity.this, R.layout.row, task);
+                ArrayList<String> data;
+                data = db.getTaskContent();
+                db.close();
+            }
+
+        });
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+
+                for (int i = 0; i < data.size(); i++) {
+
+                    Log.d("Database Content", i +". "+data.get(i));
+
+                    txt += i + ". " + data.get(i) + "\n";
+
+                }
+
+                tvResults.setText(txt);
+
+            }
+
+        });
+        DBHelper db = new DBHelper(MainActivity.this);
+
+        task = db.getTasks();
+
+        aa = new TaskAdapter(MainActivity.this, R.layout.row, task);
+
+        lv.setAdapter(aa);
+
+    }
+
+}
---
+++ app/src/main/java/sg/edu/rp/soi/demodatabase/TaskAdapter.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.soi.demodatabase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+public class TaskAdapter extends ArrayAdapter<Task> {
+    private ArrayList<Task> tasks;
+    private Context context;
+    private TextView tvID, tvDesc, tvDate;
+    public TaskAdapter(Context context, int resource, ArrayList<Task> objects) {
+        super(context, resource, objects);
+        tasks = objects;
+        this.context = context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+        tvID = rowView.findViewById(R.id.tvID);
+        tvDesc = rowView.findViewById(R.id.tvDESC);
+        tvDate = rowView.findViewById(R.id.tvDate);
+        Task currPosition = tasks.get(position);
+        tvID.setText(" "+currPosition.getId());
+        tvDesc.setText(" "+currPosition.getDescription());
+        tvDate.setText(" "+currPosition.getDate());
+        return rowView;
+    }
+}
